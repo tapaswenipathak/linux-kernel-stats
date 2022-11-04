c2779ed95497454fe8883590362b02f25c23e696
Kavita Meena <kavita23meena.2002@gmail.com>
2022-11-04 19:55:09 +0530

    updated loc.sh

--- scripts/loc.sh
+++ scripts/loc.sh
@@ -22,8 +22,13 @@ for ((i=0;i<=20;i++)) ; do
 	find -type f -name '*.[chS]' -exec wc -l {} \; | awk 'BEGIN{lines=0}{lines+=$1}END{print lines}'
 done
 
-for ((i=0;i<=1;i++)) ; do
+for ((i=0;i<=19;i++)) ; do
 	echo v5.$i
 	git checkout -fq v5.$i
 	find -type f -name '*.[chS]' -exec wc -l {} \; | awk 'BEGIN{lines=0}{lines+=$1}END{print lines}'
 done
+
+
+echo v6.0
+git checkout -fq v6.0
+find -type f -name '*.[chS]' -exec wc -l {} \; | awk 'BEGIN{lines=0}{lines+=$1}END{print lines}'
