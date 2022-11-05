ea1824d5de21c018e38ef257a6a21cbf357a5008
madhup01 <Patelmadhu06@gmail.com>
2022-11-05 22:15:40 +0530

    Added drivers_file_number.sh file

---
+++ scripts/drivers_file_number.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SRCDIR=~/torvalds/linux
+
+# Files Number in drivers
+cd $SRCDIR
+
+for ((i=12;i<=39;i++)) ; do
+	echo v2.6.$i 
+	git checkout -fq v2.6.$i
+	echo "files in drivers      `find drivers -type f | wc -l`"
+done
