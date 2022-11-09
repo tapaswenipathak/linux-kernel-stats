2ae0aed6929959f8a9aa24fcd3d88a12bbbd359e
Madhu Patel <54492585+Madhupatel08@users.noreply.github.com>
2022-11-09 14:56:47 +0530

    Update driver.sh

--- scripts/driver.sh
+++ scripts/driver.sh
@@ -1,31 +1,51 @@
 #!/bin/bash
+SRCDIR=~/linux-stable/
 
-SRCDIR=~/torvalds/linux
-
-# Number of directories in each version 
+# Number of Directories in drivers
 cd $SRCDIR
-
 
 for ((i=12;i<=39;i++)) ; do
 	echo v2.6.$i 
 	git checkout -fq v2.6.$i
-	(ls -1 | wc -l )
+	if [ $i == 12 ]; then
+	cd drivers
+	fi
+	location=
+	ls -d */ | wc -l
 done
 
 for ((i=0;i<=19;i++)) ; do
 	echo v3.$i
 	git checkout -fq v3.$i
-	(ls -1 | wc -l )
+	if [ $i == 0 ]; then
+	cd drivers
+	fi
+	ls -d */ | wc -l
 done
 
 for ((i=0;i<=20;i++)) ; do
 	echo v4.$i 
 	git checkout -fq v4.$i
-	(ls -1 | wc -l )
+	if [ $i == 0 ]; then
+	cd drivers
+	fi
+	ls -d */ | wc -l
 done
 
 for ((i=0;i<=19;i++)) ; do
 	echo v5.$i 
 	git checkout -fq v5.$i
-	(ls -1 | wc -l)
+	if [ $i == 0 ]; then
+	cd drivers
+	fi
+	ls -d */ | wc -l
+done
+
+for ((i=1; i<=7; i++)); do
+    echo v6.0.$i
+    git checkout -fq v6.0.$i
+    if [ $i == 1 ]; then
+	cd drivers
+	fi
+	ls -d */ | wc -l
 done
