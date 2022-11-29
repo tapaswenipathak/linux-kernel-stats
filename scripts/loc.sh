e368d263ffe625a3aac5a96eee3621b186db487f
Kavita Meena <kavita23meena.2002@gmail.com>
2022-11-29 12:32:35 +0530

    Updated loc.sh file including all versions of linux kernel

--- scripts/loc.sh
+++ scripts/loc.sh
@@ -12,8 +12,6 @@ n=${#all_versions[@]}
 for ((i=0; i<=$n; i++)); do
     git checkout -fq ${all_versions[$i]}
     if [[ $? -eq 0 ]]; then
-        # echo "${all_versions[$i]}"
-        # paste <(echo "${all_versions[$(($i-1))]}..${all_versions[$i]}") <(echo "$number_of_patches") | column -s $'\t' -t
         paste <(echo "${all_versions[$i]}") <(find -type f -name '*.[chS]' -exec wc -l {} \; | awk 'BEGIN{lines=0}{lines+=$1}END{print lines}') | column -s $'\t' -t
     else
         continue
