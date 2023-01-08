9a69abc4e5721ec1755f1b7f9f32c215095b79c9
Kavita Meena <kavita23meena.2002@gmail.com>
2023-01-09 00:04:29 +0530

    Added a new subsystems.sh file

---
+++ scripts/subsystems.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SRCDIR=~/linux-stable/linux-stable
+cd $SRCDIR
+
+#declaring an array containing all versions
+declare -a all_versions=($(git tag -l | sort -V))  
+
+#total no. of versions
+n=${#all_versions[@]}  
+
+for ((i=0; i<=$n; i++)); do
+    git checkout -fq ${all_versions[$i]}
+    if [[ $? -eq 0 ]]; then
+        echo ""
+        echo "${all_versions[$i]}"
+        for d in arch block crypto drivers fs init ipc kernel lib mm net security sound virt ; do
+        echo -ne "$d\t"
+	    echo "$(find $d -type f -name '*.[chS]' -exec wc -l {} \; | awk 'BEGIN{sum=0}{sum+=$1}END{print sum}')"
+        done
+        echo
+    else
+        continue
+    fi   
+done
