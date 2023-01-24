13ff3db3fad3da6c3dfe6fb847b66b3a4b247eb4
Kavita Meena <72209620+KavitaMeena23@users.noreply.github.com>
2023-01-24 20:00:40 +0530

    Updated commits.sh (#67)
    displays commits of all versions of linux kernel (v0.01 to latest)

--- scripts/commits.sh
+++ scripts/commits.sh
@@ -1,18 +1,98 @@
 #!/bin/bash
 
-SRCDIR=~/linux-stable/linux-stable
-cd $SRCDIR
+SRCDIR_a=~/archive
+cd $SRCDIR_a
 
-#declaring an array containing all versions
-declare -a all_versions=($(git tag -l | sort -V))  
+#declaring an array containing versions
+declare -a versions=($(git tag -l | sort -V))  
 
 #total no. of versions
-n=${#all_versions[@]}  
+n=${#versions[@]}  
 
-for ((i=0; i<=$n; i++)); do
-    git checkout -fq ${all_versions[$i]}
+for ((i=0; i<$n; i++)); do
+    git checkout -fq ${versions[$i]}
     if [[ $? -eq 0 ]]; then
-        echo "${all_versions[$i]}"
+        echo "${versions[$i]}"
+        git log
+    else
+        continue
+    fi
+done
+
+
+SRCDIR_b=~/erofs-utils
+cd $SRCDIR_b
+
+#declaring an array containing versions
+declare -a versions=($(git tag -l | sort -V))  
+
+#total no. of versions
+n=${#versions[@]}  
+
+for ((i=1; i<$n; i++)); do
+     git checkout -fq ${versions[$i]}
+    if [[ $? -eq 0 ]]; then
+        echo "${versions[$i]}"
+        git log
+    else
+        continue
+    fi
+done
+
+
+SRCDIR_c=~/kbd
+cd $SRCDIR_c
+
+#declaring an array containing  versions
+declare -a versions=($(git tag -l | sort -V))  
+
+#total no. of versions
+n=${#versions[@]} 
+
+for ((i=39; i<46; i++)); do
+     git checkout -fq ${versions[$i]}
+    if [[ $? -eq 0 ]]; then
+        echo "${versions[$i]}"
+        git log
+    else
+        continue
+    fi
+done
+
+
+SRCDIR_d=~/history
+cd $SRCDIR_d
+
+#declaring an array containing versions
+declare -a versions=($(git tag -l | sort -V))  
+
+#total no. of versions
+n=${#versions[@]}
+
+for ((i=9; i<168; i++)); do
+     git checkout -fq ${versions[$i]}
+    if [[ $? -eq 0 ]]; then
+        echo "${versions[$i]}"
+        git log
+    else
+        continue
+    fi
+done
+
+
+SRCDIR_e=~/linux-stable/linux-stable
+cd $SRCDIR_e
+
+#declaring an array containing versions
+declare -a versions=($(git tag -l | sort -V))  
+
+#total no. of versions
+n=${#versions[@]}  
+
+for ((i=2; i<=$n; i++)); do
+    git checkout -fq ${versions[$i]}
+    if [[ $? -eq 0 ]]; then
+        echo "${versions[$i]}"
         git log
     else
         continue
