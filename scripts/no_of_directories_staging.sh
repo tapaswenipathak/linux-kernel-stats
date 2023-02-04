4ca7f5c7781e74478efe0b859d0fd1c0183fc10b
madhup01 <Patelmadhu06@gmail.com>
2023-02-04 17:13:06 +0530

    added staging+makefile+data

---
+++ scripts/no_of_directories_staging.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+
+SRCDIR_a=~/archive
+cd $SRCDIR_a
+    #declaring an array containing all versions
+    declare -a all_versions=($(git tag -l | sort -V))  
+
+    #total no. of versions
+    n=${#all_versions[@]}  
+
+    for ((i=0; i<$n; i++)); do
+        git checkout -fq ${all_versions[$i]}
+        SRCDIR_b=$SRCDIR_a/drivers
+        if [ -d "$SRCDIR_b" ]; then 
+            cd $SRCDIR_b
+            SRCDIR="$SRCDIR_b/staging"
+                if [ -d "$SRCDIR" ] && [ $? -eq 0 ]; then
+                    cd $SRCDIR
+                    echo ${all_versions[$i]}
+                    ls -ld */ | wc -l
+                else
+                    paste <(echo "${all_versions[$i]}", "no staging directory")
+                fi
+            continue
+        else
+            paste <(echo "${all_versions[$i]}", "no drivers directory")
+        fi
+    done >> /home/madhu/Desktop/Research_Work/linux-kernel-stats/data_dir/no_of_directories_staging.sh
+
+SRCDIR_a=~/erofs-utils
+cd $SRCDIR_a
+    #declaring an array containing all versions
+    declare -a all_versions=($(git tag -l | sort -V))  
+
+    #total no. of versions
+    n=${#all_versions[@]}  
+
+    for ((i=0; i<$n; i++)); do
+        git checkout -fq ${all_versions[$i]}
+        SRCDIR_b=$SRCDIR_a/drivers
+        if [ -d "$SRCDIR_b" ]; then 
+            cd $SRCDIR_b
+            SRCDIR="$SRCDIR_b/staging"
+                if [ -d "$SRCDIR" ] && [ $? -eq 0 ]; then
+                    cd $SRCDIR
+                    echo ${all_versions[$i]}
+                    ls -ld */ | wc -l
+                else
+                    paste <(echo "${all_versions[$i]}", "no staging directory")
+                fi
+            continue
+        else
+            paste <(echo "${all_versions[$i]}", "no drivers directory")
+        fi
+    done >> /home/madhu/Desktop/Research_Work/linux-kernel-stats/data_dir/no_of_directories_staging.sh
+
+SRCDIR_a=~/kbd
+cd $SRCDIR_a
+    #declaring an array containing all versions
+    declare -a all_versions=($(git tag -l | sort -V))  
+
+    #total no. of versions
+    n=${#all_versions[@]}  
+
+    for ((i=39; i<46; i++)); do
+        git checkout -fq ${all_versions[$i]}
+        SRCDIR_b=$SRCDIR_a/drivers
+        if [ -d "$SRCDIR_b" ]; then 
+            cd $SRCDIR_b
+            SRCDIR="$SRCDIR_b/staging"
+                if [ -d "$SRCDIR" ] && [ $? -eq 0 ]; then
+                    cd $SRCDIR
+                    echo ${all_versions[$i]}
+                    ls -ld */ | wc -l
+                else
+                    paste <(echo "${all_versions[$i]}", "no staging directory")
+                fi
+            continue
+        else
+            paste <(echo "${all_versions[$i]}", "no drivers directory")
+        fi
+    done >> /home/madhu/Desktop/Research_Work/linux-kernel-stats/data_dir/no_of_directories_staging.sh
+
+SRCDIR_a=~/history
+cd $SRCDIR_a
+    #declaring an array containing all versions
+    declare -a all_versions=($(git tag -l | sort -V))  
+
+    #total no. of versions
+    n=${#all_versions[@]}  
+
+    for ((i=9; i<168; i++)); do
+        git checkout -fq ${all_versions[$i]}
+        SRCDIR_b=$SRCDIR_a/drivers
+        if [ -d "$SRCDIR_b" ]; then 
+            cd $SRCDIR_b
+            SRCDIR="$SRCDIR_b/staging"
+                if [ -d "$SRCDIR" ] && [ $? -eq 0 ]; then
+                    cd $SRCDIR
+                    echo ${all_versions[$i]}
+                    ls -ld */ | wc -l
+                else
+                    paste <(echo "${all_versions[$i]}", "no staging directory")
+                fi
+            continue
+        else
+            paste <(echo "${all_versions[$i]}", "no drivers directory")
+        fi
+    done >> /home/madhu/Desktop/Research_Work/linux-kernel-stats/data_dir/no_of_directories_staging.sh
+
+
+SRCDIR_a=~/linux-stable/linux-stable
+cd $SRCDIR_a
+    #declaring an array containing all versions
+    declare -a all_versions=($(git tag -l | sort -V))  
+
+    #total no. of versions
+    n=${#all_versions[@]}  
+
+    for ((i=0; i<$n; i++)); do
+        git checkout -fq ${all_versions[$i]}
+        SRCDIR_b=$SRCDIR_a/drivers
+        if [ -d "$SRCDIR_b" ]; then 
+            cd $SRCDIR_b
+            SRCDIR="$SRCDIR_b/staging"
+                if [ -d "$SRCDIR" ] && [ $? -eq 0 ]; then
+                    cd $SRCDIR
+                    echo ${all_versions[$i]}
+                    ls -ld */ | wc -l
+                else
+                    paste <(echo "${all_versions[$i]}", "no staging directory")
+                fi
+            continue
+        else
+            paste <(echo "${all_versions[$i]}", "no drivers directory")
+        fi
+    done >> /home/madhu/Desktop/Research_Work/linux-kernel-stats/data_dir/no_of_directories_staging.sh
+
+
+
