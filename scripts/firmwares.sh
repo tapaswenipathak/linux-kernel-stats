436d0a1d28f0da15c2b5b06ee790f58c7bb9e889
Abhishek Dutt <duttabhishek0@gmail.com>
2023-02-17 20:56:07 +0530

    Updated firmwares.sh

--- scripts/firmwares.sh
+++ scripts/firmwares.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# for v2.0 to v6.0
+# for v2.6.39.4 to v6.0
 SRCDIR=~/linux-stable
+# Directory to store the text extracted from grep command
 DIRNAME="Firmware_raw"
+
 cd $SRCDIR
 if [ ! -d "~/linux-kernel-stats/data_dir/$DIRNAME" ]; then
   # Create the directory if it doesn't exist
@@ -14,7 +16,7 @@ fi
 
 # Checkout to the specific tag
 git checkout -fq refs/tags/v2.6.39.4
-grep -q -r "fwname" > ~/linux-kernel-stats/data_dir/$DIRNAME/2.0.txt
+grep -q -r "fwname" > ~/linux-kernel-stats/data_dir/$DIRNAME/2.6.39.4.txt
 
 for((i=3; i<=6; i++)); do
 	git checkout -fq refs/tags/v$i.0
