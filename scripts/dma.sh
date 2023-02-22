0db33d18580f4e7ed9e38c85092cf629aaf1663a
madhup01 <Patelmadhu06@gmail.com>
2023-02-23 02:40:53 +0530

    added dma script

---
+++ scripts/dma.sh
@@ -0,0 +1,27 @@
+SRCDIR_e=~/linux-stable/linux-stable
+cd $SRCDIR_e
+
+myArray=("dma_pool_alloc" "dma_pool_free" "dma_pool_destroy"  "dma_pool_create" "dma_map_*" "dma_addr_t" "dma_set_mask_and_coherent" "dma_set_mask" "dma_set_coherent_mask" "DMA_TO_DEVICE" "DMA_FROM_DEVICE" "dma_mapping_error" "dma_map_page" "dma_api" "dma-api" "DMA_API" "DMA-API" "DMADEVICES" "dma_buf" "dma_buffer" "DMA_ENGINE" "DMA_VIRTUAL_CHANNELS")
+
+for ((i=3; i<=6; i++)); do
+    git checkout -fq v$i.0
+    if [[ $? -eq 0 ]]; then
+        for string in ${myArray[@]}; do
+           if [ -n "$(git log --all --grep="$string")" ]; then 
+                echo -e "\e[6;35m \n v$i.0 \n \e[0m"
+                #file_name="${string}_${all_versions[$i]}.txt"
+                echo -e "\e[6;35m \n ${string} \n \e[0m"
+                git log --all --grep="$string"
+           else
+                echo -e "\e[6;35m \n v$i.0 \n \e[0m"
+                echo "No such string exists in version v$i.0 in the git log." 
+                continue
+           fi
+        done 
+    else
+        continue
+    fi
+done 
+
+#   
+# 
