4839164a3f3e6852ded2c6d490e0d13bdd6434a9
Dheeraj Reddy <77443480+reddheeraj@users.noreply.github.com>
2023-02-27 20:56:47 +0530

    Update message_passing.sh

--- scripts/message_passing.sh
+++ scripts/message_passing.sh
@@ -15,15 +15,31 @@ declare -a search_terms=(
   "RPC"
   "shared memory"
   "semaphore"
+  "asynchronous messaging"
+  "message protocol"
+  "multicast"
+  "broadcast"
+  "ZeroMQ"
+  "JMS"
+  "MPI"
+  "rendezvous"
+  "SCTP"
+  "sctp_association"
+  "sctp_sendmsg"
+  "tipc"
+  "netlink"
+  "netlink_unicast"
 )
 
 cd ~/archive
 
 echo "Searching for message passing in Linux version v1.0"
-git checkout "v1.0"
+git checkout -fq "v1.0"
 
 for term in "${search_terms[@]}"; do
-    git log --all --grep="$term" > "../mps_gitlogs/v1.0_$term.gitlog.txt"
+    if [ ! -f "../mps_gitlogs/v1.0_$term.gitlog.txt" ]; then
+      git log --all --grep="$term" > "../mps_gitlogs/v1.0_$term.gitlog.txt"
+    fi
 done
 
 cd ..
@@ -31,10 +47,12 @@ cd ..
 cd ~/kbd
 
 echo "Searching for message passing in Linux version v2.0"
-git checkout "v2.0" &> /dev/null
+git checkout -fq "2.0.0" 
 
 for term in "${search_terms[@]}"; do
-    git log --all --grep="$term" > "../mps_gitlogs/v2.0_$term.gitlog.txt"
+    if [ ! -f "../mps_gitlogs/v2.0_$term.gitlog.txt" ]; then
+      git log --all --grep="$term" > "../mps_gitlogs/v2.0_$term.gitlog.txt"
+    fi
 done
 
 cd ..
@@ -48,11 +66,16 @@ for ((i=3; i<=6; i++)); do
   echo "Searching for message passing in Linux version v$i.0"
   
   # Check out the kernel version
-  git checkout -q "v$i.0" &> /dev/null
+  git checkout -fq "v$i.0" &> /dev/null
 
   # Loop through the search terms and search the git log for each term
   for term in "${search_terms[@]}"; do
-    git log --all --grep="$term" > "../mps_gitlogs/v$i.0_$term.gitlog.txt"
+    filename="v$i.0_$term.gitlog.txt"
+    
+    if [ ! -f "../mps_gitlogs/$filename" ]; then
+      git log --all --grep="$term" > "../mps_gitlogs/$filename"
+    fi
+    #git log --all --grep="$term" > "../mps_gitlogs/v$i.0_$term.gitlog.txt"
   done
 done
 
@@ -61,12 +84,12 @@ done
 echo "Done searching git logs"
 git checkout master &> /dev/null
 
-cd ~/mps_gitlogs
-for file in *; do
-    if [ ! -s "$file" ]; then
-        rm "$file"
-    fi
-done
+#cd ~/dhtest/mps_gitlogs
+#for file in *; do
+#    if [ ! -s "$file" ]; then
+#        rm "$file"
+#    fi
+#done
 
 
 
