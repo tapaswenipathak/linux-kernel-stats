3ba46a67447f2fffc305cc6ab6a2e3df1afc108a
Madhu Patel <54492585+Madhupatel08@users.noreply.github.com>
2023-03-04 14:54:11 +0530

    Update bluetooth.sh

--- scripts/bluetooth.sh
+++ scripts/bluetooth.sh
@@ -1,7 +1,7 @@
-SRCDIR_e=~/linux-stable/linux-stable
-cd $SRCDIR_e
+#!/bin/bash
+cd ~/linux-stable/linux-stable
 
-myArray=("*bluetooth*" "*BLUETOOTH*" "BLUETOOTH_VER*")
+myArray=("bluetooth" "BLUETOOTH" "BLUETOOTH_VER")
 
 for ((i=3; i<=6; i++)); do
     git checkout -fq v$i.0
@@ -9,11 +9,9 @@ for ((i=3; i<=6; i++)); do
         for string in ${myArray[@]}; do
            if [ -n "$(git log --all --grep="$string")" ]; then 
                 echo -e "\e[6;35m \n v$i.0 \n \e[0m"
-                #file_name="${string}_${all_versions[$i]}.txt"
                 echo -e "\e[6;35m \n ${string} \n \e[0m"
                 git log --all --grep="$string" 
            else
-                echo "hi"
                 echo -e "\e[6;35m \n v$i.0 \n \e[0m"
                 echo "No such string exists in version v$i.0 in the git log." 
                 continue
@@ -23,4 +21,38 @@ for ((i=3; i<=6; i++)); do
         continue
     fi
 done 
+
+cd ..
+
+cd ~/kbd
+    git checkout 2.0.0
+    for string in ${myArray[@]}; do
+        if [ -n "$(git log --all --grep="$string")" ]; then 
+            echo -e "\e[6;35m \n version 2.0.0 \n \e[0m"
+            echo -e "\e[6;35m \n ${string} \n \e[0m" 
+            git log --all --grep="$string"
+         else
+            echo -e "\e[6;35m \n version 2.0.0 \n \e[0m"
+            echo "No such string exists in version 2.0.0 in the git log."
+            continue
+        fi
+    done 
+   
+cd ..
+
+cd ~/archive
+
+    git checkout v1.0
+    
+    for string in ${myArray[@]}; do
+        if [ -n "$(git log --all --grep="$string")" ]; then 
+            echo -e "\e[6;35m \n v$i.0 \n \e[0m"
+            echo -e "\e[6;35m \n ${string} \n \e[0m"
+            git log --all --grep="$string" 
+         else
+            echo -e "\e[6;35m \n v$i.0 \n \e[0m"
+            echo "No such string exists in version v$i.0 in the git log." 
+        fi
+    done 
+   
 
