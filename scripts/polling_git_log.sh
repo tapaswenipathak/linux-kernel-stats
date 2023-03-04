3495d2750f60f92bb3e2a47233ec0b384e426033
Abhishek Dutt <duttabhishek0@gmail.com>
2023-03-04 22:37:36 +0530

    Minor Change

--- scripts/polling_git_log.sh
+++ scripts/polling_git_log.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-if [ ! -d ~/linux-kernel-research/linux-kernel-stats/data_dir/polling_data_dump/ ];then
-    mkdir ~/linux-kernel-research/linux-kernel-stats/data_dir/polling_data_dump/
+if [ ! -d ~/linux-kernel-stats/data_dir/polling_data_dump/ ];then
+    mkdir ~/linux-kernel-stats/data_dir/polling_data_dump/
     echo "Working.."
 else
     echo "Working..."
 fi
 
 keywordArray=(
-    # "poll"
-    # "polling"
-    # "poll_wait"
-    # "epoll"
-    # "epoll_wait"
-    # "select"
-    # "poll_initwait"
-    # "poll_schedule_timeout"
-    # "poll_freewait"
-    # "interrupt handler"
-    # "interrupt"
-    # "waitqueue"
-    # "tasklet"
-    # "Workqueue"
+    "poll"
+    "polling"
+    "poll_wait"
+    "epoll"
+    "epoll_wait"
+    "select"
+    "poll_initwait"
+    "poll_schedule_timeout"
+    "poll_freewait"
+    "interrupt handler"
+    "interrupt"
+    "waitqueue"
+    "tasklet"
+    "Workqueue"
     "poll_idle"
     "poll_freewait"
     "poll_schedule_timeout"
@@ -32,16 +32,16 @@ keywordArray=(
     )
 
 # for v1.0
-SRCDIR_1=~/linux-kernel-research/erofs-utils/
+SRCDIR_1=~/erofs-utils/
 cd $SRCDIR_1
 
 git checkout -fq "v1.0"
 for keyword in "${keywordArray[@]}"; do
     if [ -n "$(git log --all --grep="$keyword" --regexp-ignore-case)" ];then 
-            if [ ! -f ~/linux-kernel-research/linux-kernel-stats/data_dir/polling_data_dump/$keyword_v1.0.txt ]; then
+            if [ ! -f ~/linux-kernel-stats/data_dir/polling_data_dump/$keyword_v1.0.txt ]; then
                 echo -e "\e[6;35m \n v1.0 \n \e[0m"
                 file_name="${keyword}_v1.0.txt"
-                git log --all --grep="$keyword" > ~/linux-kernel-research/linux-kernel-stats/data_dir/polling_data_dump/$file_name
+                git log --all --grep="$keyword" > ~/linux-kernel-stats/data_dir/polling_data_dump/$file_name
             fi    
     else
        echo -e "\e[6;35m \n v1.0 \n \e[0m"
@@ -52,16 +52,16 @@ done
 cd ..
 
 # for v2.0
-SRCDIR_2=~/linux-kernel-research/kbd/
+SRCDIR_2=~/kbd/
 cd $SRCDIR_2
 
 git checkout -fq "2.0.0"
 for keyword in "${keywordArray[@]}"; do
     if [ -n "$(git log --all --grep="$keyword" --regexp-ignore-case)" ];then 
-        if [ ! -f ~/linux-kernel-research/linux-kernel-stats/data_dir/polling_data_dump/$keyword_v2.0.txt ]; then
+        if [ ! -f ~/linux-kernel-stats/data_dir/polling_data_dump/$keyword_v2.0.txt ]; then
             echo -e "\e[6;35m \n v2.0 \n \e[0m"
             file_name="${keyword}_v2.0.txt"
-            git log --all --grep="$keyword" > ~/linux-kernel-research/linux-kernel-stats/data_dir/polling_data_dump/$file_name
+            git log --all --grep="$keyword" > ~/linux-kernel-stats/data_dir/polling_data_dump/$file_name
         fi
     else
        echo -e "\e[6;35m \n v2.0 \n \e[0m"
@@ -72,7 +72,7 @@ done
 cd ..
 
 # for v3.0 to v6.0
-SRCDIR_3=~/linux-kernel-research/linux-stable/
+SRCDIR_3=~/linux-stable/
 cd $SRCDIR_3
 
 #declaring an array containing all versions
@@ -86,10 +86,10 @@ for ((i=n-1; i>=0; --i)); do
     if [[ $? -eq 0 ]]; then
         for keyword in ${keywordArray[@]}; do
            if [ -n "$(git log --all --grep="$keyword" --regexp-ignore-case)" ];then
-                if [ ! -f ~/linux-kernel-research/linux-kernel-stats/data_dir/polling_data_dump/$keyword_${all_versions[$i]}.txt ]; then
+                if [ ! -f ~/linux-kernel-stats/data_dir/polling_data_dump/$keyword_${all_versions[$i]}.txt ]; then
                     echo -e "\e[6;35m \n ${all_versions[$i]} \n \e[0m"
                     file_name="${keyword}_${all_versions[$i]}.txt"
-                    git log --all --grep="$keyword" > ~/linux-kernel-research/linux-kernel-stats/data_dir/polling_data_dump/$file_name
+                    git log --all --grep="$keyword" > ~/linux-kernel-stats/data_dir/polling_data_dump/$file_name
                 fi
            else
                echo -e "\e[6;35m \n ${all_versions[$i]}\n \e[0m"
