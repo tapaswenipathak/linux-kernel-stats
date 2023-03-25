294b7c0c8b42713aed973c9f846b00ad5039ef43
Abhishek Dutt <duttabhishek0@gmail.com>
2023-03-25 19:21:35 +0530

    Minor change

---
+++ scripts/Extended Scripts/driver.sh
@@ -0,0 +1,17 @@
+# version targetted = v6.2-rc1
+
+
+#!/bin/bash
+SRCDIR=~/linux-stable/linux-stable
+cd $SRCDIR/drivers
+
+
+ver_name="v6.2-rc1"
+git checkout ${ver_name}
+
+for d in $(find ~/linux-stable/linux-stable/drivers/ -maxdepth 1 -type d | sed -e '/.*drivers\/$/d' | sort) ; do
+    echo -ne "$(basename $d)\t"
+    echo "$(find $d -type f -name '*.[chS]' -exec wc -l {} \; | awk 'BEGIN{sum=0}{sum+=$1}END{print sum}')"
+done | sort -nr -k2
+
+
