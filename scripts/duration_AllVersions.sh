76f8ac9b46ad82b20811c02538c99c32ef6e319c
Dheeraj Reddy <77443480+reddheeraj@users.noreply.github.com>
2023-04-06 19:37:12 +0530

    Create duration_AllVersions.sh

---
+++ scripts/duration_AllVersions.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# outputs the duration for all versions, including rc versions.
+# Contributor: meetdheerajreddy@gmail.com
+
+filename="duration_allversions.txt"
+
+# change directory to the repository path
+cd ~/linux-kernel-stats/archive
+    #echo "archive:"
+    previous_date=""
+    previous_tag=""
+    for tag in $(git tag | grep -vE '^[0-9]+\.' | sort -V); do
+        current_date=$(git log --pretty=format:%cd --date=short -n 1 -- "$tag")
+        if [ -n "$previous_date" ]; then
+            duration=$(( ($(date -d "$current_date" +%s) - $(date -d "$previous_date" +%s)) / 86400 )) || duration=""
+            echo "$dir:$previous_tag to $tag - ${duration:-unknown} days" >> ~/linux-kernel-stats/data_dir/$filename
+        fi
+        previous_date="$current_date"
+        previous_tag="$tag"
+    done
+    for tag in $(git tag | grep -E 'rc[0-9]+' | sort -V); do
+        current_date=$(git log --pretty=format:%cd --date=short -n 1 -- "$tag")
+        if [ -n "$previous_date" ]; then
+            duration=$(( ($(date -d "$current_date" +%s) - $(date -d "$previous_date" +%s)) / 86400 )) || duration=""
+            echo "$dir:$previous_tag to $tag - ${duration:-unknown} days" >> ~/linux-kernel-stats/data_dir/$filename
+        fi
+        previous_date="$current_date"
+        previous_tag="$tag"
+    done
+
+
+
+# Second loop for erofs-utils repo
+cd ~/linux-kernel-stats/erofs-utils
+    #echo "erofs-utils:"
+    previous_date=""
+    previous_tag=""
+    for tag in $(git tag | grep -vE '^[0-9]+\.' | sort -V); do
+        current_date=$(git log --pretty=format:%cd --date=short -n 1 -- "$tag")
+        if [ -n "$previous_date" ]; then
+            duration=$(( ($(date -d "$current_date" +%s) - $(date -d "$previous_date" +%s)) / 86400 )) || duration=""
+            echo "$dir:$previous_tag to $tag - ${duration:-unknown} days" >> ~/linux-kernel-stats/data_dir/$filename
+        fi
+        previous_date="$current_date"
+        previous_tag="$tag"
+    done
+    for tag in $(git tag | grep -E 'rc[0-9]+' | sort -V); do
+        current_date=$(git log --pretty=format:%cd --date=short -n 1 -- "$tag")
+        if [ -n "$previous_date" ]; then
+            duration=$(( ($(date -d "$current_date" +%s) - $(date -d "$previous_date" +%s)) / 86400 )) || duration=""
+            echo "$dir:$previous_tag to $tag - ${duration:-unknown} days" >> ~/linux-kernel-stats/data_dir/$filename
+        fi
+        previous_date="$current_date"
+        previous_tag="$tag"
+    done
+
+
+
+for dir in ~/linux-kernel-stats/kbd ~/linux-kernel-stats/history ~/linux-kernel-stats/linux-stable; do
+    #echo "$dir: "
+    cd "$dir"
+    previous_date=""
+    previous_tag=""
+    for tag in $(git tag | grep -vE '^[0-9]+\.' | sort -V); do
+        current_date=$(git log --pretty=format:%cd --date=short -n 1 "$tag")
+        if [ -n "$previous_date" ]; then
+            duration=$(( ($(date -d "$current_date" +%s) - $(date -d "$previous_date" +%s)) / 86400 ))
+            echo "$previous_tag to $tag - $duration days" >> ~/linux-kernel-stats/data_dir/$filename
+        fi
+        previous_date="$current_date"
+        previous_tag="$tag"
+    done
+    for tag in $(git tag | grep -E 'rc[0-9]+' | sort -V); do
+        current_date=$(git log --pretty=format:%cd --date=short -n 1 "$tag")
+        if [ -n "$previous_date" ]; then
+            duration=$(( ($(date -d "$current_date" +%s) - $(date -d "$previous_date" +%s)) / 86400 ))
+            echo "$previous_tag to $tag - $duration days" >> ~/linux-kernel-stats/data_dir/$filename
+        fi
+        previous_date="$current_date"
+        previous_tag="$tag"
+    done
+done
+
