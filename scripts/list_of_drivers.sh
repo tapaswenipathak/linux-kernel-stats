0f7a7c0747a6325f1247ea294c9f32259035d4ac
Abhishek Dutt <56694152+duttabhishek0@users.noreply.github.com>
2023-04-18 20:12:55 +0530

    Fixed get_shortlogs (#245)
    - Fixes Issue #225

--- scripts/list_of_drivers.sh
+++ scripts/list_of_drivers.sh
@@ -4,21 +4,27 @@
 # Contributor: patelmadhu06@gmail.com
 
 SRCDIR=~/linux-stable
-cd $SRCDIR/drivers
+if [ -d "$SRCDIR/drivers" ]; then
+  cd "$SRCDIR/drivers"
+elif [ -d "$SRCDIR/linux-stable/drivers" ]; then
+  cd "$SRCDIR/linux-stable/drivers"
+else
+  echo "Directory not found"
+  exit 1
+fi
 
 #declaring an array containing all versions
 declare -a all_versions=($(git tag -l | sort -V))  
 
 #total no. of versions
-n=${#all_versions[@]}  
-
+n=${#all_versions[@]} 
 
 for ((i=0; i<=$n; i++)); do
     git checkout -fq ${all_versions[$i]}
     if [[ $? -eq 0 ]]; then
         echo ""
         echo ${all_versions[$i]}
-        for d in $(find ~/linux-stable/drivers/ -maxdepth 1 -type d | sed -e '/.*drivers\/$/d' | sort) ; do
+        for d in $(find . -maxdepth 1 -type d | sed -e '/.*drivers\/$/d' | sort) ; do
             echo "$(basename $d)"
         done 
     else
